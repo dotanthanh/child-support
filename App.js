c71eee0c8ddcbef5e83275c55f320043317454ee
Thanh Do <thanh.do@digia.com>
2018-10-03 02:26:52 +0300

    initial layout login page
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, KeyboardAvoidingView } from 'react-native';
+import { Text, FormLabel, FormInput, Button } from 'react-native-elements';
 
 export default class App extends React.Component {
+  state = {
+    showImage: true
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text h2 style={styles.title}>PregMind</Text>
+        <Image source={require('./assets/pregnancy.png')} style={styles.image} />
+        <KeyboardAvoidingView style={styles.form} behavior="padding" on>
+          <FormLabel>Email</FormLabel>
+          <FormInput require />
+          <FormLabel>Password</FormLabel>
+          <FormInput secureTextEntry required />
+          <View style={styles.buttonGroup}>
+            <Button title="Register" />
+            <Button title="Login" />
+          </View>
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -17,5 +33,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 36,
+    paddingRight: 36 
   },
+  title: {
+    marginTop: 24,
+    letterSpacing: 2
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginVertical: 24
+  },
+  form: {
+    maxWidth: 300
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 24
+  }
 });
