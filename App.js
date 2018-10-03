4ce3ed8e91ae6d143d31de7a821db50e9a1ca0ab
Thanh Do <thanh.do@digia.com>
2018-10-04 00:27:53 +0300

    handle layout, keyboard and dummy login
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- App.js
+++ App.js
@@ -1,57 +1,26 @@
 import React from 'react';
 import { StyleSheet, View, Image, KeyboardAvoidingView } from 'react-native';
 import { Text, FormLabel, FormInput, Button } from 'react-native-elements';
+import { createStackNavigator } from 'react-navigation';
+import { LoginScreen } from './src/LoginScreen';
 
 export default class App extends React.Component {
-  state = {
-    showImage: true
-  };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text h2 style={styles.title}>PregMind</Text>
-        <Image source={require('./assets/pregnancy.png')} style={styles.image} />
-        <KeyboardAvoidingView style={styles.form} behavior="padding" on>
-          <FormLabel>Email</FormLabel>
-          <FormInput require />
-          <FormLabel>Password</FormLabel>
-          <FormInput secureTextEntry required />
-          <View style={styles.buttonGroup}>
-            <Button title="Register" />
-            <Button title="Login" />
-          </View>
-        </KeyboardAvoidingView>
-      </View>
+      <RootStacks />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft: 36,
-    paddingRight: 36 
+const RootStacks = createStackNavigator(
+  {
+    Landing: {
+      screen: LoginScreen,
+      navigationOptions: { header: null }
+    }
   },
-  title: {
-    marginTop: 24,
-    letterSpacing: 2
-  },
-  image: {
-    height: 200,
-    width: 200,
-    borderRadius: 100,
-    marginVertical: 24
-  },
-  form: {
-    maxWidth: 300
-  },
-  buttonGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginVertical: 24
+  {
+    initialRouteName: 'Landing'
   }
-});
+);
